1f5dd8b98ccb14011d77995fe37d6c6984714311
David Hoppe <hopasaurus@gmail.com>
2019-03-14 21:05:55 -0400

    Added red blue and green

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Use children
+class Red extends Component {
+  render() {
+    return <span className={"Red"}>{this.props.children}</span>
+  }
+}
+
+// Use attribute
+class Blue extends Component {
+  render() {
+    return <span className={"Blue"}>{this.props.content}</span>
+  }
+}
+
+// stateless component:
+const Green = props => (<span className={"Green"}>{props.children}</span>)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>word <Red>word</Red> word</div>
+        <div>word <Blue content={"word"}/> word</div>
+        <div>word <Green>word</Green> word</div>
       </div>
     );
   }
